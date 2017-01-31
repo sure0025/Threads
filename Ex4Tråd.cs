66137a8ccc978d595ee49dfef0d82a03b0252f37
sure0025 <sure0025@edu.eal.dk>
2017-01-31 11:45:38 +0100

    Add project files.

---
+++ Ex4Tråd.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+// Ilustrerer problemerne omkring tråde og synkronisering, hvis flere tråde arbejder på samme objekt
+
+namespace Trådeksempel4
+{
+    public class Ex4Tråd
+    {
+        private int threadCount = 0;
+        private int count = 0;
+        private char sign; 
+        private int number;
+        public Ex4Tråd(char sign, int count)
+        {
+            this.count = count;
+            this.sign = sign;
+        }
+        public void RunThread()
+        {
+            // ------------------------------------------------
+            // Her kan komme synkroniseringsproblem - dog ikke så ofte
+            // da det kun er meget lidt kode der kan afbrydes i
+            // Here you get synchronization problem - though not as often 
+            // as it is only very little code that can be interrupted for
+            int forward = threadCount;
+            char wsign = (char)(((int)sign) + forward); // tegn ændres ved flere tråde på samme object
+            // signs changed by multiple threads at the same object
+            threadCount++;
+            // ------------------------------------------------
+
+            Random random = new Random();
+
+            for (int gange = 0; gange < count; gange++)
+            {
+                System.Console.Write(wsign);
+                for (int x = 0; x < 20000000; x++) ;       // lav en forsinkelse aktiv. Low delay active
+
+                //--------------------------------------------------------
+                // Her kan forekomme synkroniserings problemer. Here occur synchronization problems
+                // pausen fremprovokerer for tydeligheden. break provokes for clarity
+                number = number + 1;                              // synkroniserings problem skabes. synchronization problem created
+                Thread.Sleep(random.Next(1, 20));            // lav en forsinkelse pasiv. Low delay passive
+                number = number - 1;
+                wsign = (char)(((int)wsign) + number);         // da tal skulle være 0. as the number should be 0
+                // skulle wtegn ikke ændres. should not be changed wtegn
+                //--------------------------------------------------------
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+// Ilustrerer problemerne omkring tråde og synkronisering, hvis flere tråde arbejder på samme objekt
+// Ilustrerer problems of threads and synchronization, if multiple threads working on the same object
+
+namespace Trådeksempel4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Start");
+
+            Ex4Tråd tk1 = new Ex4Tråd('.', 80);
+            Ex4Tråd tk2 = new Ex4Tråd('X', 50);
+            Thread t1 = new Thread(tk1.RunThread);
+            Thread t1b = new Thread(tk1.RunThread);       // Ny tråd på samme objekt. New thread on the same object
+            Thread t2 = new Thread(tk2.RunThread);
+
+            t1.Start();
+            t1b.Start();
+            t2.Start();
+            //                      Thread.Sleep(5);
+
+            t2.Join();
+            t1.Join();
+            t1b.Join();
+            System.Console.WriteLine("\nEnter for Exit");
+            System.Console.ReadLine();
+            System.Console.WriteLine("EXIT");
+
+        }
+    }
+}
